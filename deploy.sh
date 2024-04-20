cd5e3636bd95227fca8fbcfe9bb03cdb98fd73c1
Dylan Medina <medinabaez1120@gmail.com>
2024-04-19 22:29:53 -0500

    2024-04-19 22:29:53

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 # Set the server details
 SERVER="ubuntu@got.jemavi.co"
 REMOTE_PATH="/home/ubuntu/got/django_hivik"
-LOCAL_PATH="C:\Users\medin\Desktop\django_hivik"
+LOCAL_PATH="/c/Users/medin/Desktop/django_hivik"
 cd "$LOCAL_PATH"
 git add .
 git commit -m "$(date +"%Y-%m-%d %H:%M:%S")"
 git push
-ssh -i C:\Users\medin\Desktop\GOT-SERPORT.pem "$SERVER" "cd '$REMOTE_PATH' && git pull"
+ssh -i GOT-SERPORT.pem "$SERVER" "cd '$REMOTE_PATH' && git pull"
